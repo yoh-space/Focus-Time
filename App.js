e250c092f50ce55c118835fcc662dcd247b5e1a5
yoh-space <yohannesdamtie822@gmail.com>
2026-03-01 22:38:42 +0300

    Adding new FocusTime screen component , creating different hooks , and passing props between App.js and Focus.Time screen component

--- App.js
+++ App.js
@@ -1,9 +1,37 @@
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import { SafeAreaView  } from 'react-native-safe-area-context';
 import {TextInput } from 'react-native-paper';
-
+import { useState } from 'react';
+import FocusTime from './components/FocusTime';
 
 export default function App(){
+
+  const [switchScreen , setSwitchScreen ] = useState(false);
+  const [task , setTask ] = useState("");
+  const [tasks, setTasks] = useState([]); 
+  const [selectedTask, setSelectedTask] = useState("");
+
+
+  const changeScreen = () => {
+    setSwitchScreen(!switchScreen);
+  }
+
+  const addTask = ( ) => {
+    const trimmed = task.trim();
+
+    if (trimmed.length > 0)
+    {
+      setTasks( prev =>[...prev, trimmed]);
+      setTask("");
+      setSelectedTask(trimmed);
+
+    }
+  }
+
+  if(switchScreen){
+    return <FocusTime focusTask={selectedTask} onBack={changeScreen}/>
+  }
+
 
   return(
     <SafeAreaView style={styles.container}>
@@ -12,19 +40,27 @@ export default function App(){
           placeholder = "What would you like to focus ..."
           mode = {"outlined"}
           label = "Focus"
-          style = {styles.InputText}  
+          style = {styles.InputText} 
+          value = {task}
+          onChangeText = {(text)=> setTask(text)}
+
         />
-        <TouchableOpacity style={styles.fabButton} onPress={()=>{}}>
+        <TouchableOpacity style={styles.fabButton} onPress={() => {
+          addTask();
+          changeScreen();
+        }}>
           <Text style={styles.fabText}>+</Text>
         </TouchableOpacity>
       </View>
 
       <View style={styles.focusedTasks}>
         <Text style={styles.focusTitle}>  Things we've focused on: </Text>
+        {tasks.map((task,index) => (
+          <Text key={index} style={styles.taskText}>{task}</Text>
+        ))}
         
         <View style={{padding: 20}}>
-          <Text style={{ fontSize: 18 , color: 'white', fontWeight: 'semi-bold'}}> 1, Learn react native </Text>
-          <Text style={{ fontSize: 18 , color: 'white', fontWeight: 'semi-bold'}}> 2, Learn JS basics </Text>
+
         </View>
 
       </View>
@@ -67,6 +103,11 @@ const styles = StyleSheet.create({
     fontSize: 22,
     marginLeft: 10,
     color : 'white'
+  },
+  taskText: {
+    fontWeight: 'semi-bold',
+    frontSize: 18, 
+    color: 'white',
+    padding: 10,
   }
-
 })
