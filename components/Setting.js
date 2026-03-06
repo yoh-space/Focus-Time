41aa062e11d158fa7865d29ef5fc8fa75801dbe5
yoh-space <yohannesdamtie822@gmail.com>
2026-03-07 00:19:57 +0300

    creating new setting screen

---
+++ components/Setting.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function Setting(){
+    return(
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text style={{fontSize: 24, fontWeight: 'bold'}}>Settings</Text>
+        </View>
+    )
+}
